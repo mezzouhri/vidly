a908a93c5475cc739c55dd31f0a58fda2eb2e16d
Mohcine EZZOUHRI <ezzouhri.dev@gmail.com>
2019-11-19 23:14:28 +0100

    Firest commit by Mohcine

---
+++ src/components/movies.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    this.setState({
+      movies: this.state.movies.filter(m => m._id !== movie._id)
+    });
+  };
+
+  render() {
+    const { movies } = this.state;
+    const { length: count } = movies;
+    if (count === 0) return <p>There are no movies in the database</p>;
+
+    return (
+      <React.Fragment>
+        <p>Showing {movies.length} movies in the database :</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;

50400025841216d1454d1fdb01a49382e6cf7e72
Mohcine EZZOUHRI <ezzouhri.dev@gmail.com>
2019-12-20 23:17:38 +0100

    add branch withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Movies from "./components/movies";
 import "./App.css";
+import NavBar from "./components/navBar";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container paddingTop">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 

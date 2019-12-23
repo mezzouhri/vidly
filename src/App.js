71d09cdd32026fcb2e717e4dd201d8ff5593c779
Mohcine EZZOUHRI <ezzouhri.dev@gmail.com>
2019-12-23 14:54:38 +0100

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
 
 function App() {
   return (
     <React.Fragment>
       <NavBar />
-      <main className="container paddingTop">
+      <main className="container">
         <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
